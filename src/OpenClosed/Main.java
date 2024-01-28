05276f71f6cdd0f174735c292a1257701c3e78a4
Jamie Symonds <jamie.symonds@gmail.com>
2024-01-27 23:36:22 -0400

    Added Open-Closed examples

---
+++ src/OpenClosed/Main.java
@@ -0,0 +1,12 @@
+package OpenClosed;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Circle circ = new Circle(55);
+        System.out.println(circ.area());
+
+        Rectangle rect = new Rectangle(5, 6);
+        System.out.println(rect.area());
+    }
+}
